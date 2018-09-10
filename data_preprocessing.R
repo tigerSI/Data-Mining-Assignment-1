011dbff5064f5a56fcceb0f127ac2f1020b819fd
Sirapop Issariyodom <sirapop.tiger@gmail.com>
2018-09-10 21:13:57 +0700

    update

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -10,6 +10,7 @@ summary(MyData$mig_prev_sunbelt)
 #view plot of two variables
 #plot(MyData$age, MyData$class_worker)
 
+#delete column because there are too much missing data
 cleaned_data  <- MyData
 cleaned_data$mig_prev_sunbelt <- NULL
 cleaned_data$mig_chg_msa <- NULL
@@ -19,6 +20,7 @@ cleaned_data <- distinct(cleaned_data)
 
 summary(cleaned_data)
 
+#eleminate some missing data in those 3 column because there are aournd 8000 objects
 cleaned_data$country_father = as.character(cleaned_data$country_father)
 cleaned_data$country_mother = as.character(cleaned_data$country_mother)
 cleaned_data$country_self = as.character(cleaned_data$country_self)
@@ -39,4 +41,38 @@ cleaned_data$country_father = as.factor(cleaned_data$country_father)
 cleaned_data$country_mother = as.factor(cleaned_data$country_mother)
 cleaned_data$country_self = as.factor(cleaned_data$country_self)
 
+# change NA to Do not know
+cleaned_data$hisp_origin = as.character(cleaned_data$hisp_origin)
+for(i in 1:nrow(cleaned_data)){
+  if(cleaned_data$hisp_origin[i] == " NA"){
+    cleaned_data$hisp_origin[i] <- " Do not know"
+  }
+}
+cleaned_data$hisp_origin = as.factor(cleaned_data$hisp_origin)
+
+# change ? to not in universe because not in universe has too much data and ? should be in this column without change the data pattern 
+cleaned_data$state_prev_res = as.character(cleaned_data$state_prev_res)
+for(i in 1:nrow(cleaned_data)){
+  if(cleaned_data$state_prev_res[i] == " ?"){
+    cleaned_data$state_prev_res[i] <- " Not in universe"
+  }
+}
+cleaned_data$state_prev_res = as.factor(cleaned_data$state_prev_res)
+
 summary(cleaned_data)
+
+get_mode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+mean(cleaned_data$det_hh_summ)
+median(cleaned_data$capital_gains)
+get_mode(cleaned_data$capital_losses)
+sd(cleaned_data$capital_losses)
+range(cleaned_data$capital_losses)
+
+summary(cleaned_data$capital_losses)
+
+cleaned_data$class_worker = as.character(cleaned_data$class_worker)
+hist(cleaned_data$class_worker)
